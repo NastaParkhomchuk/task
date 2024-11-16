f2cf94180bcb6f3b73bc681321df492a839d2304
Anastasiia <nastaparkhomchuk@gmail.com>
2024-11-17 01:40:25 +0200

    the last try

--- Desktop/task/pages/LoginPage.js
+++ Desktop/task/pages/LoginPage.js
@@ -1,24 +1,64 @@
-/* global browser, $ */
+// /* global browser, $ */
 
-class LoginPage {
-  get usernameField() { return $('#user-name'); }
-  get passwordField() { return $('#password'); } 
-  get loginButton() { return $('#login-button'); } 
-  get errorMessage() { return $('.error-message-container'); }
+// class LoginPage {
+//   get usernameField() { return $('#user-name'); }
+//   get passwordField() { return $('#password'); } 
+//   get loginButton() { return $('#login-button'); } 
+//   get errorMessage() { return $('.error-message-container'); }
   
-    async login(username, password) {
-      await this.usernameField.setValue(username);
-      await this.passwordField.setValue(password);
-      await this.loginButton.click();
+//     async login(username, password) {
+//       await this.usernameField.setValue(username);
+//       await this.passwordField.setValue(password);
+//       await this.loginButton.click();
+//     }
+  
+//     async clearField(field) {
+//       await field.click(); 
+//       await browser.keys(['Control', 'a']); 
+//       await browser.keys('Backspace'); 
+//   }
+// }
+
+  
+//   module.exports = new LoginPage();
+const { $ } = require('@wdio/globals')
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get inputUsername () {
+        return $('#username');
     }
-  
-    async clearField(field) {
-      await field.click(); 
-      await browser.keys(['Control', 'a']); 
-      await browser.keys('Backspace'); 
-  }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnSubmit () {
+        return $('button[type="submit"]');
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open('login');
+    }
 }
 
-  
-  module.exports = new LoginPage();
-  
+module.exports = new LoginPage();
--- Desktop/task/test/specs/test.e2e.js
+++ Desktop/task/test/specs/test.e2e.js
@@ -1,15 +1,31 @@
-const { expect } = require('@wdio/globals')
-const LoginPage = require('../pageobjects/login.page')
-const SecurePage = require('../pageobjects/secure.page')
+// const { expect } = require('@wdio/globals')
+// const LoginPage = require('../pageobjects/login.page')
+// const SecurePage = require('../pageobjects/secure.page')
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
+// describe('My Login application', () => {
+//     it('should login with valid credentials', async () => {
+//         await LoginPage.open()
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!')
-    })
-})
+//         await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+//         await expect(SecurePage.flashAlert).toBeExisting()
+//         await expect(SecurePage.flashAlert).toHaveTextContaining(
+//             'You logged into a secure area!')
+//     })
+// })
+const { $ } = require('@wdio/globals')
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class SecurePage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get flashAlert () {
+        return $('#flash');
+    }
+}
+
+module.exports = new SecurePage();
 
